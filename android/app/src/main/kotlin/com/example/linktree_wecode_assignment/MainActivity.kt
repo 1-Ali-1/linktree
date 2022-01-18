f30da61c4ef6b236a83f017a2e4709eff74632eb
1-Ali-1 <Alifarhad.engineer@gmail.com>
2022-01-18 17:54:27 +0300

    link tree

---
+++ android/app/src/main/kotlin/com/example/linktree_wecode_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.linktree_wecode_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Map<String, String> map = {
+  'facebook': 'https://www.facebook.com/ali.farhad.98',
+  'linkedIn': 'https://www.linkedin.com/in/ali-farhad-90b4b8198/',
+  'github': 'https://github.com/1-Ali-1'
+};
+
+String name = 'Ali Farhad Ali';
+String bio = 'Study Software Engineering in the Salahaddin university';
+String location = 'Erbil , qarya new village';
+
+Map<IconData, String> mapContact = {
+  FontAwesomeIcons.whatsapp: '+9647502408969',
+  FontAwesomeIcons.viber: '+9647502408969'
+};
